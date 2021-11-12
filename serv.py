afd331d3fff76d4f86f1488593e73b245633acc5
charlie melnarik <Charlie.melnarik@gmail.com>
2021-11-12 16:27:12 -0700

    test

--- serv.py
+++ serv.py
@@ -5,9 +5,14 @@ app = Flask(__name__)
 @app.route('/')
 def home():
    return render_template('testingwebcam.html')
+
+@app.route('/')
 def west():
    return render_template('cooke.html')
+
+app.route('/')
 def cooke():
    return render_template('west.html')
+
 if __name__ == '__main__':
    app.run(host="0.0.0.0", port=80, debug=True)
